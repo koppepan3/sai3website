fb5de8851c31e45646da63c16997de0afcd6de91
github-haruki <64785351+github-haruki@users.noreply.github.com>
2021-11-08 21:01:48 +0900

    range

--- slicksettings.js
+++ slicksettings.js
@@ -9,14 +9,14 @@
 		dots: true,//下部ドットナビゲーションの表示
 		responsive: [
 			{
-			breakpoint: 600,//モニターの横幅が769px以下の見せ方
+			breakpoint: 850,//モニターの横幅が769px以下の見せ方
 			settings: {
 				slidesToShow: 2,//スライドを画面に2枚見せる
 				slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
 			}
 		},
 		{
-			breakpoint: 600,//モニターの横幅が426px以下の見せ方
+			breakpoint: 850,//モニターの横幅が426px以下の見せ方
 			settings: {
 				slidesToShow: 1,//スライドを画面に1枚見せる
 				slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
